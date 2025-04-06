255c53df1b081c1047484a499806750f4f43830f
Usaid-Khan <165344421+Usaid-Khan@users.noreply.github.com>
2025-04-06 08:22:01 +0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -128,7 +128,7 @@ async function main() {
 
     // Adding an event listener for close button
     document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".left").style.left = "100%"
+        document.querySelector(".left").style.left = "-100%"
         document.querySelector(".playbar").style.bottom = "15px"
     });
 
